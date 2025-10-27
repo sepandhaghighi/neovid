90492c67e42bb6f544f7bcd28b7db54dd818aeda
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-10-28 02:48:11 +0330

    `saveRecent` bug fix (#16)
    * fix : saveRecent function bug fixed
    
    * doc : CHANGELOG.md updated

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function saveRecent(title, video, videoType, subtitle="", subtitleType="url", pr
   let recent = JSON.parse(localStorage.getItem(recentKey) || "[]");
 
 
-  recent = recent.filter(item => !(item.video===video && item.subtitle===subtitle));
+  recent = recent.filter(item => !(item.video===video));
 
   recent.unshift({title, video, videoType, subtitle, subtitleType, progress});
   if(recent.length>5) recent = recent.slice(0,5);
