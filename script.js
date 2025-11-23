21461a66e37749e4bff2804d4e836484a1266c34
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-11-23 14:29:43 +0330

    Refactor (#39)
    * fix : reloading section refactored
    
    * fix : reloading section refactored

--- script.js
+++ script.js
@@ -146,15 +146,21 @@ function renderRecent(){
     li.appendChild(spanProgress);
 
     li.addEventListener("click", ()=>{
-      if(item.videoType==="url" && (!item.subtitle || item.subtitleType==="url")){
-        playVideo(item.video, item.subtitle, item.title, item.videoType);
-        saveRecent(item.title, item.video, item.videoType, item.subtitle, item.subtitleType);
+      if(item.videoType==="url"){
         videoUrl.value = item.video;
-        if (item.subtitle && item.subtitleType==="url"){
-            subtitleUrl.value = item.subtitle;
+      }
+      else{
+        videoUrl.value = "";
+        alert("Please reselect the local video.");
+      }
+      if (item.subtitle){
+        if (item.subtitleType==="url"){
+          subtitleUrl.value = item.subtitle;    
         }
-      } else {
-        alert("Please reselect the local video/subtitle to play it again.");
+        else{
+          subtitleUrl.value = "";
+          alert("Please reselect the local subtitle.");
+        }
       }
     });
 
