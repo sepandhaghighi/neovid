895771c862f1e75e0289a328947c4a84384bfe96
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-11-30 11:55:29 +0330

    feat : remove button added

--- script.js
+++ script.js
@@ -118,8 +118,12 @@ function renderRecent(){
   recent.forEach(item=>{
     const li = document.createElement("li");
     const spanTitle = document.createElement("span");
+    const spanRemove = document.createElement("span");
     li.style.background = getProgressBackground(item.progress);
-    spanTitle.textContent=truncateTitle(item.title, maxLimit);
+    spanTitle.textContent = truncateTitle(item.title, maxLimit);
+    spanTitle.className = "recent-title";
+    spanRemove.textContent = "🗑️";
+    spanRemove.className = "recent-remove";
     if(item.videoType==="url"){
       const tag = document.createElement("span");
       tag.className="url-tag";
@@ -154,10 +158,17 @@ function renderRecent(){
     spanProgress.className="recent-progress";
     spanProgress.textContent = `${item.progress||0}%`;
 
+    li.appendChild(spanRemove);
     li.appendChild(spanTitle);
     li.appendChild(spanProgress);
 
-    li.addEventListener("click", ()=>{
+    spanRemove.addEventListener("click", ()=>{
+      let newRecent = JSON.parse(localStorage.getItem(recentKey) || "[]");
+      newRecent = newRecent.filter(recentItem => !(recentItem.title===item.title));
+      localStorage.setItem(recentKey, JSON.stringify(newRecent));
+      renderRecent();
+    });
+    spanTitle.addEventListener("click", ()=>{
       let isDataLoaded = false;
       if(item.videoType==="url"){
         videoUrl.value = item.video;
