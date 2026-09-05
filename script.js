e2de8b66a442b59c7cef8fbdace469baad2b84da
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-09-05 12:57:17 +0330

    feat : add End shortcut

--- script.js
+++ script.js
@@ -756,5 +756,11 @@ document.addEventListener("keydown", (event) => {
         }
       }
       break;
+
+    case "End":
+      if (DOM.player.duration) {
+        DOM.player.currentTime = DOM.player.duration;
+      }
+      break;
   }
 });
